2fbfc567f6583ea8f4e818dbd4a52b79bbccd562
Aishaan Datt <aishaandatt12@gmail.com>
2026-06-28 04:21:59 +0530

    Add connection pool config for Vercel

--- lib/db.ts
+++ lib/db.ts
@@ -2,5 +2,10 @@ import postgres from "postgres"
 import { drizzle } from "drizzle-orm/postgres-js"
 import * as schema from "./schema"
 
-const client = postgres(process.env.DATABASE_URL!, { ssl: "require" })
+const client = postgres(process.env.DATABASE_URL!, {
+  ssl: "require",
+  max: 3,
+  idle_timeout: 20,
+  connect_timeout: 10,
+})
 export const db = drizzle(client, { schema })
